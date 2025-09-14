b10c5df2099f3cbc223c49181a9fc753a8f3c6e7
kamlakar19feb <kamlakar19feb@gmail.com>
2025-09-14 21:38:26 +0530

    Update python_problems_day5.py

--- Day6/python_problems_day5.py
+++ Day6/python_problems_day5.py
@@ -1,4 +1,4 @@
-1.	Reverse a string without using slicing ([::-1]).
+#1.	Reverse a string without using slicing ([::-1]).
 Example: "hello" → "olleh"
 
 string1=input("Enter string1: ")
@@ -6,3 +6,19 @@ rev = ""
 for char in range(len(string1)-1,-1,-1):
   rev=rev+string1[char]
 print("reverse string1: ", rev)
+
+#2.	Find the second largest number in a list.
+Input: [12, 45, 67, 23, 89, 45] → Output: 67
+nums = list(map(int, input("Enter space separated numbers: ").split()))
+
+largest = max(nums)
+second_largest = float("-inf")
+
+for i in nums:
+    if i != largest and i > second_largest:
+        second_largest = i
+
+if second_largest == float("-inf"):
+    print("No second largest (all numbers are the same)")
+else:
+    print("Second largest =", second_largest)
