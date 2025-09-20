157e7c519ac5f0dbcc3a2258060616554fbddb70
kamlakar19feb <kamlakar19feb@gmail.com>
2025-09-20 18:43:31 +0530

    Create second_largest.py

---
+++ Day_08/Python/second_largest.py
@@ -0,0 +1,13 @@
+Find the second largest number in a list (without using max twice)
+👉 Input: [10, 20, 4, 45, 99] → Output: 45.
+
+nums = list(map(int, input("Enter space separated numbers: ").split()))
+largest=max(nums)
+second_largest=float("-inf")
+for i in nums:
+  if i!=largest and i>second_largest:
+    second_largest=i
+if second_largest==float("-inf"):
+  print("No second largest, all numbers are same")
+else:
+  print("Second largest =", second_largest)
